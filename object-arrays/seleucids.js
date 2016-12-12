dc71c29b2ea068d19ac3193c08db2447deca90e5
Eric Sowell <eric.sowell@gmail.com>
2016-12-12 07:34:23 -0600

    seleucids object array

---
+++ object-arrays/seleucids.js
@@ -0,0 +1,102 @@
+//Data taken from https://en.wikipedia.org/wiki/List_of_Seleucid_rulers
+
+var apiResult = {
+  rulers: [
+    {
+      name: 'Seleucus I Nicator',
+      startReign: 305,
+      endReign: 281,
+      consort: 'Apama'
+    },
+    {
+      name: 'Antiochus I Soter',
+      startReign: 281,
+      endReign: 261,
+      consort: 'Stratonice of Syria'
+    },
+    {
+      name: 'Antiochus II Theos',
+      startReign: 261,
+      endReign: 246,
+      consort: 'Laodice I'
+    },
+    {
+      name: 'Seleucus II Callinicus',
+      startReign: 246,
+      endReign: 225,
+      consort: 'Laodice'
+    },
+    {
+      name: 'Seleucus III Ceraunus',
+      startReign: 225,
+      endReign: 223,
+      consort: ''
+    },
+    {
+      name: 'Antiochus III the Great',
+      startReign: 223,
+      endReign: 187,
+      consort: 'Laodice III'
+    },
+    {
+      name: 'Seleucus IV Philopator',
+      startReign: 187,
+      endReign: 175,
+      consort: 'Laodice IV'
+    },
+    {
+      name: 'Antiochus IV Epiphanes',
+      startReign: 175,
+      endReign: 163,
+      consort: 'Laodice IV'
+    },
+    {
+      name: 'Antiochus V Eupator',
+      startReign: 163,
+      endReign: 161,
+      consort: ''
+    },
+    {
+      name: 'Demetrius I Soter',
+      startReign: 161,
+      endReign: 150,
+      consort: 'Apama'
+    },
+    {
+      name: 'Alexander I Balas',
+      startReign: 150,
+      endReign: 145,
+      consort: 'Cleopatra Thea'
+    },
+    {
+      name: 'Demetrius II Nicator',
+      startReign: 145,
+      endReign: 138,
+      consort: 'Cleopatra Thea'
+    }
+  ]
+}
+
+
+
+//write a function that returns all kings named "Seleucus"
+
+//write a function that returns all kings named "Antiochus"
+
+//Once those two are complete, refactor so that they can both use the same function
+
+//-------------------------
+
+//write a function that counts the rulers with a consort named "Laodice"
+
+//write a function that counts the rulers with a consort named "Apama"
+
+//Once those two are complete, refactor so that they can both use the same function
+
+//-------------------------
+
+//write a function that returns the rulers who reigned more than five years
+
+//write a function that returns the rulers who reigned more than twenty years
+
+//Once those two are complete, refactor so that they can both use the same function
